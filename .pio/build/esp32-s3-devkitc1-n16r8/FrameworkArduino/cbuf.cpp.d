5c9a8634d3687dfc5d767fcfa885ef0124252243
SamuelCicuitStudio <134279941+SamuelCicuitStudio@users.noreply.github.com>
2025-11-01 04:14:45 +0100

    last working version

---
+++ .pio/build/esp32-s3-devkitc1-n16r8/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32-s3-devkitc1-n16r8/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/Samuel\ Tshibangu/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/Samuel\ Tshibangu/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/esp32-s3-devkitc1-n16r8/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32-s3-devkitc1-n16r8/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/Samuel\ Tshibangu/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/Samuel\ Tshibangu/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
